d4c230a60424cc7cc64a806db50e99f07fb0f9ea
Minibr <minimin9823@gmail.com>
2025-04-23 22:52:25 +0900

    fix:블필요 코드 제거

--- java-calculator/src/main/java/com/calculator/service/CalculatorService.java
+++ java-calculator/src/main/java/com/calculator/service/CalculatorService.java
@@ -28,8 +28,6 @@ public class CalculatorService {
      */
     public double multiply(double a, double b) {
         return a * b;
-        // 미구현 상태로 예외 발생
-        //throw new UnsupportedOperationException("곱하기 기능은 아직 구현되지 않았습니다.");
     }
 
     /**
